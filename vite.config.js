8099c00480fe1e6c83f40952c67df136f54f2fc1
KakumanuLakshmiNarayana <avinari32@gmail.com>
2025-09-15 19:02:56 -0500

    Resolve merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-=======
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // <-- Try this alternative!
+  base: './', // <-- Try this alternative!
 });
->>>>>>> 0eef8b00a1f23920f0e235bac060df9768b0ca49
